80f8bf9cbe971663705e9c52cd109907ac116b83
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-09-28 21:42:58 -0400

    created and ran unit testing for pastebin class

---
+++ src/app/pastebin.spec.ts
@@ -0,0 +1,23 @@
+// import from Pastebin class
+import { Pastebin } from './pastebin';
+
+describe('Pastebin', () => {
+  it('should create an instance of Pastebin', () => {
+    expect(new Pastebin()).toBeTruthy();
+  });
+});
+
+  it('should accept values', () => {
+    let pastebin = new Pastebin();
+    pastebin = {
+      id: 111,
+      title: 'Hello World',
+      language: 'Ruby',
+      paste: 'print "Hello"',
+    };
+    expect(pastebin.id).toEqual(111);
+    expect(pastebin.language).toEqual('Ruby');
+    expect(pastebin.paste).toEqual('print "Hello"');
+  });
+
+
